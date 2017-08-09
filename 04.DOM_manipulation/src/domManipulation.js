728fec65e526c7743b872ffbb161650d9cf7f8d9
Ezio Shiki <zzyagg@gmail.com>
2017-08-09 17:58:11 +0800

    Done: Chapter4: dom manipulation

--- 04.DOM_manipulation/src/domManipulation.js
+++ 04.DOM_manipulation/src/domManipulation.js
@@ -1,3 +1,44 @@
-var dom = function(){
-  // code goes here
-};
+var dom = (function(){
+  function remove(element){
+    element.remove();
+  }
+
+  function append(target, element){
+    target.appendChild(element);
+  }
+
+  function prepend(target, element){
+    target.insertBefore(element,target.firstChild);
+  }
+
+  function after(target, element){
+    try{
+      var parent = target.parentNode;
+      parent.insertBefore(element,target.nextSibling)
+    }catch(e){
+      if(e instanceof TypeError){
+        console.log(e)
+      }else{
+        throw e
+      }
+    }
+  }
+
+  function before(target, element){
+    var parent = target.parentNode;
+    parent.insertBefore(element,target);
+  }
+
+  function val(target){
+    return target.value;
+  }
+  
+  return{
+    remove,
+    append,
+    prepend,
+    after,
+    before,
+    val,
+  }
+})();
