de9d1a68010e6f2291ca0da304ab286ffcda0000
Ezio Shiki <zzyagg@gmail.com>
2017-08-09 16:20:11 +0800

    Done: Chapter2: css manipulation

--- 02.CSS_manipulation/src/cssProp.js
+++ 02.CSS_manipulation/src/cssProp.js
@@ -1,5 +1,54 @@
 function cssProp() {
   'use strict';
+  var args = Array.from(arguments);
+  
+  // write less check logic first
 
-  //code goes here
+  if(!(args[0] instanceof HTMLElement)){
+    throw new Error('Args 1 is not a HTMLElement')
+  }
+
+  var element = args[0]
+
+  if(args.length===2){
+    if((typeof args[1]==='object')){
+      //set multiple properties
+      //cssProp(element, {cssProperty: value, cssProperty: value});
+      var props = args[1]
+      for(var prop in props){
+        setProp(element,prop,props[prop])
+      }
+
+    }else if((typeof args[1]==='string')){
+      //get CSS property value
+      //cssProp(element, cssProperty);
+      return getProp(element,args[1]);
+
+    }else {
+      throw new Error('Invalid arguments 2')
+    }
+  }else if(args.length===3){
+    if((typeof args[1]==='string')&&(typeof args[2]==='string')){
+      // set single property
+      // cssProp(element, cssProperty, value);
+      setProp(element,args[1],args[2])
+
+    }else{
+      throw new Error('Invalid arguments prop name or value')
+    }
+  }
+
+  // don't do this stupid things : e.style.prop
+  function getProp(e,prop){
+    return e.style[prop];
+  }
+
+  function setProp(e,prop,value){
+    e.style[prop] = value;
+  }
+
+  function isEmpty(str) {
+    return (!str || 0 === str.length);
+  }
+
 }
