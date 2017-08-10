6be4465df929fe08783fa7042cc3f8a21b2ae889
Ezio Shiki <zzyagg@gmail.com>
2017-08-10 14:09:51 +0800

    Mid: add baisc event handler, passed 5/15 test.
    Faced two same callback cannot be called twice, since addEventListener's
    function.

--- 06.event_listeners/src/eventListeners.js
+++ 06.event_listeners/src/eventListeners.js
@@ -1,4 +1,39 @@
 var eventListener = (function() {
   'use strict';
-  //code goes here
+
+  var listeners = [];
+
+  function on(element,event,callback){
+    element.addEventListener(event,callback);
+    listeners.push({ele:element,eve:event,c:callback});
+  }
+
+  function off(element,event,callback){
+    if(callback){
+      //removes a specific callback on an element of the event type
+
+      element.removeEventListener(event,callback);
+    }
+
+    if((!callback)&&event){
+      //removes all callbacks on an element of the event type
+      var events = listeners.filter(i=>(i.ele===element)&&(i.eve===event));
+      events.map(event=>{
+        element.removeEventListener(event.eve,event.c)
+      })
+    }
+
+    if((!callback)&&(!event)){
+      //removes all callbacks on an element
+      var events = listeners.filter(i=>(i.ele===element));
+      events.map(event=>{
+        element.removeEventListener(event.eve,event.c)
+      })
+    }
+  }
+  
+  return {
+    on,
+    off
+  }
 })();
