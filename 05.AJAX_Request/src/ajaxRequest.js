e2e0b212e05373bf75e1fc0451ea49f11077e466
Ezio Shiki <zzyagg@gmail.com>
2017-08-10 11:06:07 +0800

    REFACTOR: add some comment to AJAX request, rename some var

--- 05.AJAX_Request/src/ajaxRequest.js
+++ 05.AJAX_Request/src/ajaxRequest.js
@@ -1,16 +1,18 @@
 function ajaxReq() {
   'use strict';
+  // check args
   var args = Array.from(arguments);
   if(args[0]&&(typeof args[0] === 'string')){
     var url = args[0];
   }else{
     throw new Error('Invalid url argument')
   }
-
   var defaultOption = {
     method : "GET"
   }
   var option = args[1]?args[1]:defaultOption;
+
+  // if have data obj, build a query string
   var data = option.data;
   var dataStr = '';
   if(typeof data === 'object'){
@@ -20,51 +22,54 @@ function ajaxReq() {
   }else{
     dataStr = data;
   }
+  
   var context = option.context?option.context:option;
 
-
-  var httpRequest = new XMLHttpRequest();
-  httpRequest.onreadystatechange = function(){
-    if(httpRequest.readyState === 4){
+  // handle response callback
+  var r = new XMLHttpRequest();
+  r.onreadystatechange = function(){
+    if(r.readyState === 4){
       //recived response
-      if(httpRequest.status===200){
+      if(r.status===200){
         if(option.success) {
           option.success.call(
           context,
-          JSON.parse(httpRequest.responseText),
-          httpRequest.status,
-          httpRequest
+          JSON.parse(r.responseText),
+          r.status,
+          r
           )
         };
       }else{
         if(option.failure){
           option.failure.call(
             context,
-            httpRequest,
-            httpRequest.status,
-            httpRequest.responseText
+            r,
+            r.status,
+            r.responseText
           )
         };
       }
       if(option.complete){ 
         option.complete.call(
           context,
-          httpRequest,
-          httpRequest.status,
-          httpRequest.responseText
+          r,
+          r.status,
+          r.responseText
         )
       };
 
     }else{
-
+      // do nothing while not complete requset
     }
   }
 
+  // POST need a special header
   if(option.method==='POST'){
-    httpRequest.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    r.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
   }
 
-  httpRequest.open(option.method,url,true);
-  httpRequest.send(dataStr);
+  // Do ajax
+  r.open(option.method,url,true);
+  r.send(dataStr);
 
 }
